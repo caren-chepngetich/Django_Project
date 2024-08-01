237f9fafa6a1cf942291eb98f7da1e97b342b420
Caren Chepngetich <chepngetichkosgeicarenn@gmail.com>
2024-08-01 09:05:49 +0300

    added files

--- classperiod/models.py
+++ classperiod/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ClassPeriod(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    course = models.CharField(max_length=100)  
+    day_of_the_week = models.CharField(max_length=20)  
+    total_students = models.PositiveIntegerField()
+    trainer_name = models.CharField(max_length=100)  
+    trainers_assistant_name = models.CharField(max_length=100)  
+    number_of_hours = models.DurationField()
